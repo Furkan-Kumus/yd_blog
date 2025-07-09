b7614826fe07d72cf3319c5f592dd0dfbd67cba3
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-07-09 12:38:42 +0300

    resim falan değişti

--- gundem/src/dummydata_fixed.js
+++ gundem/src/dummydata_fixed.js
@@ -48,7 +48,7 @@ export const coursesCard = [
     coursesName: "Introducing to Software Engineering",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by John Smith",
         totalTime: "50 lectures (190 hrs)",
       },
@@ -62,7 +62,7 @@ export const coursesCard = [
     coursesName: "Enhancing Adobe Photoshop CC 2020 Skills",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Ram Gurung",
         totalTime: "30 lectures (125 hrs)",
       },
@@ -76,7 +76,7 @@ export const coursesCard = [
     coursesName: "HTML, CSS, and Javascript for Web Developers",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Saroj Nepal",
         totalTime: "50 lectures (50 hrs)",
       },
@@ -90,7 +90,7 @@ export const coursesCard = [
     coursesName: "Introducing to Programming with WordPress",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Prabin Thapa Mager",
         totalTime: "20 lectures (20 hrs)",
       },
@@ -104,7 +104,7 @@ export const coursesCard = [
     coursesName: "Introducing to Programming with ReactJS",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Sunil Bishowkaram",
         totalTime: "100 lectures (150 hrs)",
       },
@@ -118,7 +118,7 @@ export const coursesCard = [
     coursesName: "Learn Frontend Programming Language",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Sunil Bishowkaram",
         totalTime: "200 lectures (300 hrs)",
       },
@@ -132,7 +132,7 @@ export const coursesCard = [
     coursesName: "Introducing to with HTML / CSS",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Price",
         totalTime: "20 lectures (50 hrs)",
       },
@@ -146,7 +146,7 @@ export const coursesCard = [
     coursesName: "Introducing to with JAVA",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Petter",
         totalTime: "80 lectures (200 hrs)",
       },
@@ -160,7 +160,7 @@ export const coursesCard = [
     coursesName: "Introducing to with HTML / CSS / JAVASCRIPT",
     courTeacher: [
       {
-        dcover: "./images/back.webp",
+        dcover: "/yd_blog/images/back.webp",
         name: "by Petter",
         totalTime: "50 lectures (50 hrs)",
       },
@@ -177,110 +177,110 @@ export const online = [
     course: "25 Courses",
   },
   {
-    cover: "./images/courses/online/o2.png",
-    hoverCover: "./images/courses/online/o2.1.png",
+    cover: "/yd_blog/images/courses/online/o2.png",
+    hoverCover: "/yd_blog/images/courses/online/o2.1.png",
     courseName: "Art & Design",
     course: "25 Courses",
   },
   {
-    cover: "./images/courses/online/o3.png",
-    hoverCover: "./images/courses/online/o3.1.png",
+    cover: "/yd_blog/images/courses/online/o3.png",
+    hoverCover: "/yd_blog/images/courses/online/o3.1.png",
     courseName: "Computer Science",
     course: "10 Courses",
   },
   {
-    cover: "./images/courses/online/o4.png",
-    hoverCover: "./images/courses/online/o4.1.png",
+    cover: "/yd_blog/images/courses/online/o4.png",
+    hoverCover: "/yd_blog/images/courses/online/o4.1.png",
     courseName: "History & Archeologic",
     course: "15 Courses",
   },
   {
-    cover: "./images/courses/online/o5.png",
-    hoverCover: "./images/courses/online/o5.1.png",
+    cover: "/yd_blog/images/courses/online/o5.png",
+    hoverCover: "/yd_blog/images/courses/online/o5.1.png",
     courseName: "Software Engineering",
     course: "30 Courses",
   },
   {
-    cover: "./images/courses/online/o6.png",
-    hoverCover: "./images/courses/online/o6.1.png",
+    cover: "/yd_blog/images/courses/online/o6.png",
+    hoverCover: "/yd_blog/images/courses/online/o6.1.png",
     courseName: "Information Software",
     course: "60 Courses",
   },
   {
-    cover: "./images/courses/online/o7.png",
-    hoverCover: "./images/courses/online/o7.1.png",
+    cover: "/yd_blog/images/courses/online/o7.png",
+    hoverCover: "/yd_blog/images/courses/online/o7.1.png",
     courseName: "Health & Fitness",
     course: "10 Courses",
   },
   {
-    cover: "./images/courses/online/o8.png",
-    hoverCover: "./images/courses/online/o8.1.png",
+    cover: "/yd_blog/images/courses/online/o8.png",
+    hoverCover: "/yd_blog/images/courses/online/o8.1.png",
     courseName: "Marketing",
     course: "30 Courses",
   },
   {
-    cover: "./images/courses/online/o9.png",
-    hoverCover: "./images/courses/online/o9.1.png",
+    cover: "/yd_blog/images/courses/online/o9.png",
+    hoverCover: "/yd_blog/images/courses/online/o9.1.png",
     courseName: "Graphic Design",
     course: "80 Courses",
   },
   {
-    cover: "./images/courses/online/o10.png",
-    hoverCover: "./images/courses/online/o10.1.png",
+    cover: "/yd_blog/images/courses/online/o10.png",
+    hoverCover: "/yd_blog/images/courses/online/o10.1.png",
     courseName: "Music",
     course: "120 Courses",
   },
   {
-    cover: "./images/courses/online/o11.png",
-    hoverCover: "./images/courses/online/o11.1.png",
+    cover: "/yd_blog/images/courses/online/o11.png",
+    hoverCover: "/yd_blog/images/courses/online/o11.1.png",
     courseName: "Business Administration",
     course: "17 Courses",
   },
   {
-    cover: "./images/courses/online/o12.png",
-    hoverCover: "./images/courses/online/o12.1.png",
+    cover: "/yd_blog/images/courses/online/o12.png",
+    hoverCover: "/yd_blog/images/courses/online/o12.1.png",
     courseName: "Web Management",
     course: "17 Courses",
   },
 ]
 export const team = [
   {
-    cover: "./images/team/t1.webp",
+    cover: "/yd_blog/images/team/t1.webp",
     name: "Ph.D Adrian Molises",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t2.webp",
+    cover: "/yd_blog/images/team/t2.webp",
     name: "Ph.D Arthur MaGregor",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t3.webp",
+    cover: "/yd_blog/images/team/t3.webp",
     name: "Ph.D Anna Hanzen",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t4.webp",
+    cover: "/yd_blog/images/team/t4.webp",
     name: "Ph.D Brian Wooden",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t5.webp",
+    cover: "/yd_blog/images/team/t5.webp",
     name: "Ph.D Adrian Molises",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t6.webp",
+    cover: "/yd_blog/images/team/t6.webp",
     name: "Ph.D Arthur MaGregor",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t7.webp",
+    cover: "/yd_blog/images/team/t7.webp",
     name: "Ph.D Anna Hanzen",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
-    cover: "./images/team/t8.webp",
+    cover: "/yd_blog/images/team/t8.webp",
     name: "Ph.D Brian Wooden",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
@@ -350,7 +350,7 @@ export const blog = [
     com: "5 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b2.webp",
+    cover: "/yd_blog/images/blog/b2.webp",
   },
   {
     id: 3,
@@ -359,7 +359,7 @@ export const blog = [
     com: "10 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b3.webp",
+    cover: "/yd_blog/images/blog/b3.webp",
   },
   {
     id: 4,
@@ -368,7 +368,7 @@ export const blog = [
     com: "3 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b4.webp",
+    cover: "/yd_blog/images/blog/b4.webp",
   },
   {
     id: 5,
@@ -377,7 +377,7 @@ export const blog = [
     com: "3 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b5.webp",
+    cover: "/yd_blog/images/blog/b5.webp",
   },
   {
     id: 6,
@@ -386,7 +386,7 @@ export const blog = [
     com: "12 COMMENTS ",
     title: "Build your Dream Software & Engineering Career",
     desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-    cover: "../images/blog/b6.webp",
+    cover: "/yd_blog/images/blog/b6.webp",
   },
 ]
 export const testimonal = [
@@ -395,20 +395,20 @@ export const testimonal = [
     name: "ROGER SCOTT",
     post: "MARKETING MANAGER",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t1.webp",
+    cover: "/yd_blog/images/testo/t1.webp",
   },
   {
     id: 2,
     name: "ROGER SCOTT",
     post: "MARKETING MANAGER",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t2.webp",
+    cover: "/yd_blog/images/testo/t2.webp",
   },
   {
     id: 3,
     name: "ROGER SCOTT",
     post: "MARKETING MANAGER",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    cover: "./images/testo/t3.webp",
+    cover: "/yd_blog/images/testo/t3.webp",
   },
 ]
